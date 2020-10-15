75349be9e27cccb9625ade1141cd7f224703e0b8
testingsep.github.io <Tembanator@users.noreply.github.com>
2020-10-15 14:51:01 -0700

    animated portfolio

---
+++ js/animate_counters.js
@@ -0,0 +1,52 @@
+// Get the sections
+skillLoader();
+
+function skillLoader() {
+  var section = document.querySelector('#skills');
+  var counters = document.querySelectorAll('.counter');
+  var circles = document.getElementsByClassName('loading');
+  var bars = document.getElementsByClassName('front');
+
+  if (isInView(section)) {
+    animateBars(circles, bars);
+    for (var i = 0; i < counters.length; i++) {
+      animateCounters(counters[i]);
+    }
+  }
+
+}
+
+// check if section is in view
+function isInView(section) {
+  var bounding = section.getBoundingClientRect();
+  if (bounding.top >= 0 && bounding.top < 120 || bounding.bottom <= 0 && bounding.bottom > 120) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Adds animate class to circles and bars
+function animateBars(circles, bars) {
+  // When in view add class=animate
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].classList.add('animate');
+    bars[i].classList.add('animate');
+  }
+}
+
+function animateCounters(counter) {
+  var target = parseInt(counter.dataset.percentage);
+  var animationLength = 2000/target;
+  if (counter.classList.contains('waiting')) {
+    // If the increment has not started remove waiting class
+    counter.classList.remove('waiting');
+    let increment = 1;
+    let time = setInterval(function() {
+      increment += 1;
+      counter.innerHTML = increment +"%";
+      if (increment === target) {
+        clearInterval(time);
+      }
+    }, animationLength);
+  }
+}
--- js/pagination.js
+++ js/pagination.js
@@ -1,19 +1,20 @@
 function myFunction(){
-  // Get the H1 heading
+  // Get the sections
   var first = document.querySelector('#home');
   var second = document.querySelector('#services');
   var third = document.querySelector('#portfoilio');
   var fourth = document.querySelector('#resume');
   var fifth = document.querySelector('#skills');
   var sixth = document.querySelector('#contact');
-  var cn = document.querySelector('#current-number');
-
+  var currentNumber = document.querySelector('#current-number');
+   // Array of the sections
   var pages = [first, second, third, fourth, fifth, sixth];
 
+	// check if section is in view and update current number in pagination
   for (var i = 0; i < pages.length; i++){
     var bounding = pages[i].getBoundingClientRect();
     if (bounding.top >= 0 && bounding.top < 120 || bounding.bottom <= 0 && bounding.bottom > 120){
-     cn.innerHTML = "0" +(i + 1);
+     currentNumber.innerHTML = "0" +(i + 1);
     }
   }
 
